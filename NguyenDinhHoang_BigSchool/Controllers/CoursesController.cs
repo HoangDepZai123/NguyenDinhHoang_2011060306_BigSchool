cb2b944c6290e3b086a9c8af6e066a38afc29662
Hoang <Hoang@LAPTOP-VAKEB2K1>
2023-03-16 11:07:47 +0700

    Add project files.

---
+++ NguyenDinhHoang_BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using NguyenDinhHoang_BigSchool.Models;
+using NguyenDinhHoang_BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenDinhHoang_BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
